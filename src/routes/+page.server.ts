39907647f53c453e7b41d56ee44637112a5cb643
sparrowsl <benthorpe.dev@gmail.com>
2024-08-26 16:29:21 +0000

    feat: display images with correct info

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,15 @@
 import { redirect } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
+import db from "$lib/prisma";
 
 export const load: PageServerLoad = async ({ locals }) => {
-	return { user: locals.user };
+	const products = await db.product.findMany({
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+
+	return { user: locals.user, products };
 };
 
 export const actions: Actions = {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,43 @@
-<section class="min-h-[80vh] grid gap-7 grid-cols-4 py-10">
-	{#each { length: 15 } as _}
-		<figure class="shadow-xl p-5">
-			<img src="" alt="" />
-			<figcaption>
-				<h3>Lorem ipsum dolor sit amet.</h3>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, totam.
-				</p>
-			</figcaption>
-		</figure>
-	{/each}
-</section>
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+	import type { PageServerData } from "./$types";
+
+	export let data: PageServerData;
+	console.log(data);
+</script>
+
+{#if data.products}
+	<section class="min-h-[80vh] grid gap-7 grid-cols-4 py-10">
+		{#each data.products as product (product.id)}
+			<figure class="shadow-xl h-fit">
+				<img
+					src="data:image/webp;base64,{product.image}"
+					alt={product.title}
+					class="w-full max-h-60 object-cover"
+					loading="lazy"
+				/>
+				<figcaption class="p-2">
+					<h3 class="text-sm flex items-center gap-2">
+						<Icon icon="pajamas:title" />
+						{product.title}
+					</h3>
+					<p class="flex items-center gap-1 mt-2">
+						<Icon icon="mdi:attach-money" /><span class="text-xs">SLE</span>
+						{product.price}
+					</p>
+
+					<a
+						class="block w-fit border text-sm px-4 rounded-full bg-teal-500 text-white !ml-auto"
+						href="/products/{product.id}"
+					>
+						view &rightarrow;
+					</a>
+				</figcaption>
+			</figure>
+		{/each}
+	</section>
+{:else}
+	<div class="py-10">
+		<p class="text-gray-500 italic">No products has been posted for sale...</p>
+	</div>
+{/if}
